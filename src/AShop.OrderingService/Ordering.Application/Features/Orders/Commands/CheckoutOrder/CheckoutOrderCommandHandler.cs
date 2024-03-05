f4c717f00056a142e99f460c4f4bf9a168a247f0
Armin Habibi <a.habibi@netbar.org>
2024-03-05 23:13:47 +0330

    feat: added Ordering.Api services handlers and implementations and docker composes

---
+++ src/AShop.OrderingService/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+    private readonly IEmailService _emailService;
+    private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+
+    public CheckoutOrderCommandHandler
+    (
+        IOrderRepository orderRepository,
+        IMapper mapper,
+        IEmailService emailService,
+        ILogger<CheckoutOrderCommandHandler> logger
+    )
+    {
+        _orderRepository = orderRepository;
+        _mapper = mapper;
+        _emailService = emailService;
+        _logger = logger;
+    }
+
+    public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+    {
+        var orderEntity = _mapper.Map<Order>(request);
+        var newOrder = await _orderRepository.AddAsync(orderEntity);
+
+        _logger.LogInformation($"Order {newOrder.Id} is successfully created.");
+
+        await SendMail(newOrder);
+
+        return newOrder.Id;
+    }
+
+    private async Task SendMail(Order order)
+    {
+        var email = new Email(
+            "ezozkme@gmail.com",
+            "Order was created.",
+            "Order was created"
+        );
+
+        try
+        {
+            await _emailService.SendEmail(email);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Order {order.Id} failed due to an error with the mail service: {ex.Message}");
+        }
+    }
+}
